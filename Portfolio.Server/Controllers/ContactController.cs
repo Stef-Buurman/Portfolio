ea1b06a6c2eb6873c7bb57c35c292d6a3e3eedf8
Stef <st3fbuurman@gmail.com>
2025-02-03 15:39:08 +0100

    Add Contact API, remove Weather API, and update models
    Added ContactController to handle contact-related API requests with a POST endpoint for uploading contact information. Removed WeatherForecastController. Introduced EmployeeCount enum to represent employee count ranges. Created Contact model class to represent contact information. Added ContactService for business logic related to contact uploads. Introduced Globals class to manage SQLite database connection and initialization.

---
+++ Portfolio.Server/Controllers/ContactController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portfolio.Server.Filters;
+using Portfolio.Server.Models;
+using Portfolio.Server.Services;
+
+namespace Portfolio.Server.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class ContactController : ControllerBase
+    {
+        private readonly ContactService _contactService;
+
+        public ContactController(ContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        [HttpPost("contact")]
+        [ApiKeyActionFilter]
+        public IActionResult UploadContact([FromBody] Contact contact)
+        {
+            Console.WriteLine("dddddddddddddddd");
+            _contactService.UploadContact(contact);
+            return Ok();
+        }
+    }
+}
---
+++ Portfolio.Server/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using SQLite;
+
+namespace Portfolio.Server.Models
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Company { get; set; }
+        public EmployeeCount EmployeeCount { get; set; }
+        public string CompanyInformation { get; set; }
+        public string Email { get; set; }
+        public string? Phone { get; set; }
+        public string Message { get; set; }
+    }
+}
