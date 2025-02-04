a410c393533b95206efcf74b4b7ae7a8a203984e
Stef <st3fbuurman@gmail.com>
2025-02-04 15:08:21 +0100

    Add ContactListFilter for request authorization
    Introduced ContactListFilter in Portfolio.Server.Filters to handle authorization for incoming HTTP requests. The filter checks for the "Authorization" header and compares it with the value from ContactListOptions. If the header is missing or values do not match, it sets the response status to 401 (Unauthorized). Added necessary using directives for dependencies.

---
+++ Portfolio.Server/Filters/ContactListFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Options;
+using Portfolio.Server.Models;
+using System.Text.Json;
+
+namespace Portfolio.Server.Filters
+{
+    public class ContactListFilter : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext actionContext, ActionExecutionDelegate next)
+        {
+            var context = actionContext.HttpContext;
+            if (!context.Request.Headers.ContainsKey("Authorization"))
+            {
+                Console.WriteLine("--------");
+                context.Response.StatusCode = 401;
+                return;
+            }
+
+            var contactListOptions =
+              context.RequestServices.GetService<IOptions<ContactListOptions>>() switch
+              {
+                  { Value: var __ } => __,
+                  _ => new ContactListOptions() { Authorization = Guid.NewGuid().ToString() }
+              };
+            Console.WriteLine(JsonSerializer.Serialize(contactListOptions));
+            if (context.Request.Headers["Authorization"] != contactListOptions.Authorization)
+            {
+                context.Response.StatusCode = 401;
+                return;
+            }
+            await next();
+        }
+    }
+}
