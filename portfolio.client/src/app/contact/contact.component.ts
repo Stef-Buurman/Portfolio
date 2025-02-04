17ef2bdfaf308381ad5694d1701f43fa94ba7d6e
Stef <st3fbuurman@gmail.com>
2025-02-04 15:05:56 +0100

    Refactor contact form bindings and add toast notifications
    * Updated `contact.component.html` to change form field bindings from camelCase to lowercase.
    * Modified `employeeCount` dropdown to use custom `employeeEnum` pipe.
    * Updated `contact.component.ts`:
      - Removed `EmployeeCountNumeric` import.
      - Added `ToastrService` and `ToastService` imports.
      - Changed `contact` object properties to lowercase.
      - Updated `employeeCounts` array to use `EmployeeCount`.
      - Added `ToastService` to constructor.
      - Modified `onSubmit` to handle form submission with toast messages.
      - Replaced `this.contactForm.resetForm()` with `this.contactForm.onReset()`.

--- portfolio.client/src/app/contact/contact.component.ts
+++ portfolio.client/src/app/contact/contact.component.ts
@@ -1,8 +1,10 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { IContact } from '../../Interfaces/IContact';
-import { EmployeeCount, EmployeeCountNumeric } from '../../Enums/EmployeeCount';
+import { EmployeeCount } from '../../Enums/EmployeeCount';
 import { ContactService } from '../../Services/ContactService';
+import { ToastrService } from 'ngx-toastr';
+import { ToastService } from '../../Services/ToastService';
 
 @Component({
   selector: 'app-contact',
@@ -11,34 +13,39 @@ import { ContactService } from '../../Services/ContactService';
 })
 export class ContactComponent {
   contact: IContact = {
-    FirstName: '',
-    LastName: '',
-    Company: '',
-    EmployeeCount: -1,
-    CompanyInformation: '',
-    Email: '',
-    Phone: '',
-    Message: ''
+    firstName: '',
+    lastName: '',
+    company: '',
+    employeeCount: EmployeeCount.OneToTen,
+    companyInformation: '',
+    email: '',
+    phone: '',
+    message: ''
   };
 
   employeeCounts = Object.keys(EmployeeCount).filter(key => isNaN(Number(key))).map((key, index) => ({
     index: index - 1,
-    label: EmployeeCountNumeric[key as keyof typeof EmployeeCountNumeric],
+    label: EmployeeCount[key as keyof typeof EmployeeCount],
   }));
 
   @ViewChild('contactForm') contactForm!: NgForm;
-  constructor(private contactService: ContactService) { }
+  constructor(private contactService: ContactService,
+    private toastService: ToastService) { }
 
   onSubmit() {
-    console.log(this.contactForm.value);
     if (this.contactForm.valid) {
-      console.log('Form Submitted:', this.contact);
-      if (typeof this.contact.EmployeeCount === 'string') {
-        this.contact.EmployeeCount = parseInt(this.contact.EmployeeCount);
+      if (typeof this.contact.employeeCount === 'string') {
+        this.contact.employeeCount = parseInt(this.contact.employeeCount);
       }
-      this.contactService.sendContact(this.contact);
-      this.contactForm.resetForm();
-      this.contact.EmployeeCount = -1;
+      this.contactService.sendContact(this.contact).subscribe({
+        complete: () => {
+          this.toastService.showSuccess('Gelukt, uw bericht is successvol ontvangen!', 'Success');
+          this.contactForm.onReset();
+        },
+        error: (error) => {
+          this.toastService.showError('Oeps! hier ging iets niet helemaal goed. Probeer het opnieuw!', 'Error');
+        }
+      });
     }
   }
 }
