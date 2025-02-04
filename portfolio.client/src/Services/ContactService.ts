eff5e2a6d0fb3102e4f69409e7637638d49a707d
Stef <st3fbuurman@gmail.com>
2025-02-04 15:07:22 +0100

    Refactor ContactService and update imports
    - Removed unused imports: `HttpClient`, `BehaviorSubject`, and `EmployeeCount`.
    - Added `AuthorizationService` import.
    - Updated `ContactService` constructor to remove `HttpClient` dependency.
    - Refactored `sendContact` method to return `Observable<any>` and use `postRequestWithData` from `AuthorizationService`.
    - Added `getContacts` method to return `Observable<IContact[]>` using `getRequest` from `AuthorizationService`.

--- portfolio.client/src/Services/ContactService.ts
+++ portfolio.client/src/Services/ContactService.ts
@@ -1,28 +1,20 @@
-import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { AuthorizationService } from "./AuthorizationService";
 import { IContact } from "../Interfaces/IContact";
-import { EmployeeCount } from "../Enums/EmployeeCount";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  constructor(private http: HttpClient,
-    private authorizationService: AuthorizationService) {
+  constructor(private authorizationService: AuthorizationService) {
   }
 
-  sendContact(contact: IContact) {
-    console.log(contact)
-    //contact.EmployeeCount = Object.keys(EmployeeCount).indexOf(`${contact.EmployeeCount}`);
-    this.authorizationService.sendRequestWithData<IContact, IContact>('/api/contact', contact).subscribe({
-      next: (response) => {
-        console.log(response);
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    });
+  sendContact(contact: IContact):Observable<any> {
+    return this.authorizationService.postRequestWithData<IContact, IContact>('/api/contact', contact);
+  }
+
+  getContacts(apiKey:string):Observable<IContact[]> {
+    return this.authorizationService.getRequest<IContact[]>('/api/contacts', apiKey);
   }
 }
